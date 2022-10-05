faf5009c9aca5f870099a91022d8c3fc0768d67f
RyanKoech <sirryankoech@gmail.com>
2022-10-05 10:27:21 +0300

    Implemented Response datamodels

---
+++ app/src/main/java/com/example/mpesaapp/model/CallBackMetaData.kt
@@ -0,0 +1,5 @@
+package com.example.mpesaapp.model
+
+data class CallbackMetadata(
+    val Item: List<Item>
+)
--- app/src/main/java/com/example/mpesaapp/model/STKPush.kt
+++ app/src/main/java/com/example/mpesaapp/model/STKPush.kt
@@ -4,15 +4,15 @@ import com.google.gson.annotations.SerializedName
 
 
 class STKPush(
-    @field:SerializedName("BusinessShortCode") private val businessShortCode: String,
-    @field:SerializedName("Password") private val password: String,
-    @field:SerializedName("Timestamp") private val timestamp: String,
-    @field:SerializedName("TransactionType") private val transactionType: String,
-    @field:SerializedName("Amount") private val amount: String,
-    @field:SerializedName("PartyA") private val partyA: String,
-    @field:SerializedName("PartyB") private val partyB: String,
-    @field:SerializedName("PhoneNumber") private val phoneNumber: String,
-    @field:SerializedName("CallBackURL") private val callBackURL: String,
-    @field:SerializedName("AccountReference") private val accountReference: String,
-    @field:SerializedName("TransactionDesc") private val transactionDesc: String
+    @field:SerializedName("BusinessShortCode") val businessShortCode: String,
+    @field:SerializedName("Password") val password: String,
+    @field:SerializedName("Timestamp") val timestamp: String,
+    @field:SerializedName("TransactionType") val transactionType: String,
+    @field:SerializedName("Amount") val amount: String,
+    @field:SerializedName("PartyA") val partyA: String,
+    @field:SerializedName("PartyB") val partyB: String,
+    @field:SerializedName("PhoneNumber") val phoneNumber: String,
+    @field:SerializedName("CallBackURL") val callBackURL: String,
+    @field:SerializedName("AccountReference") val accountReference: String,
+    @field:SerializedName("TransactionDesc") val transactionDesc: String
 )
